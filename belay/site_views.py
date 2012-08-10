0c2b88aa82fe942b3f57dd2e1abd7e49065188d1
James Stevenson <james.stevenson@threespot.com>
2012-08-09 23:35:43 -0400

    Adds Index site view and template for it.

--- belay/site_views.py
+++ belay/site_views.py
@@ -1,4 +1,12 @@
+from django.views import generic
+
 """
 Contains views that are not specific to any application, such as the
 site index (home) page.
 """
+
+class Index(generic.TemplateView):
+    """
+    The homepage of belayapp.com
+    """
+    template_name = "index.html"
--- belay/urls.py
+++ belay/urls.py
@@ -1,16 +1,22 @@
 from django.conf import settings
-from django.conf.urls import include, patterns, static, url
-
+from django.conf.urls import include, patterns, url
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+from belay.site_views import Index
+
+
 admin.autodiscover()
 
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'belay.views.home', name='home'),
-    # url(r'^belay/', include('belay.foo.urls')),
+    url(r'^$', Index.as_view(), name="index"),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/passwords/',include('belay.utils.password_urls')
     ),
     url(r'^admin/', include(admin.site.urls)),
-) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT, show_indexes=True)
+)
+
+
+if settings.DEBUG:
+    urlpatterns += staticfiles_urlpatterns()
